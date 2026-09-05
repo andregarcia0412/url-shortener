531d447471e73d779871620d8eff899d948fffad
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-05 17:50:24 -0300

    :sparkles: feature: app header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <main className="min-h-screen bg-bg text-ink p-4">
+      <Header />
+    </main>
+  );
 }
 
 export default App;
