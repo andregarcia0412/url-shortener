7b63efb559d7f93a689a5ec0783545f20338eb47
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-04 22:50:33 -0300

    :sparkles: feature: database module and application related env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './shared/database/database.module';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        ALLOWED_ORIGINS: Joi.string().when('NODE_ENV', {
+          is: 'production',
+          then: Joi.required(),
+          otherwise: Joi.optional(),
+        }),
+
+        MONGO_USER: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+        MONGO_PORT: Joi.number().port().required(),
+        MONGO_DB: Joi.string().required(),
+        MONGO_URI: Joi.string().uri().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
