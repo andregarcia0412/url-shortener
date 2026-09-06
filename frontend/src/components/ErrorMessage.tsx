8d8514b4591e3bc78b46eadb1cf7485d5be55ca3
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-06 17:42:05 -0300

    :recycle: refactor: add images alt texts

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -7,7 +7,7 @@ type ErrorMessageProps = {
 export const ErrorMessage = ({ message }: ErrorMessageProps) => {
   return (
     <div className="flex items-center gap-2">
-      <img src={alert} height={14} width={14} />
+      <img src={alert} height={14} width={14} alt="alert" />
       <p className="text-accent-300 text-xs">{message}</p>
     </div>
   );
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,12 @@ import chain from "../assets/chain.svg";
 export const Header = () => {
   return (
     <header className="flex items-center gap-3 bg-bg p-4">
-      <img src={chain} draggable={false} className="select-none h-6 w-6" />
+      <img
+        src={chain}
+        draggable={false}
+        className="select-none h-6 w-6"
+        alt="logo"
+      />
       <h1 className="text-xl leading-none text-ink select-none [text-box:trim-both_cap_alphabetic]">
         ShortyURL
       </h1>
