5dba5b9c3521c8cdc644390e4f07d2e9689ec8fc
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-06 17:54:02 -0300

    :sparkles: feature: add icon and text props to button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,11 +1,18 @@
 import ring from "../assets/ring.svg";
 
 type ButtonProps = {
+  text: string;
   onClick: () => void;
+  icon?: string;
   loading?: boolean;
 };
 
-export const Button = ({ onClick, loading = false }: ButtonProps) => {
+export const Button = ({
+  text,
+  onClick,
+  icon,
+  loading = false,
+}: ButtonProps) => {
   return (
     <button
       className={`bg-transparent border border-accent min-h-12 px-4 rounded-md text-accent hover:bg-accent/22 cursor-pointer ${
@@ -15,8 +22,12 @@ export const Button = ({ onClick, loading = false }: ButtonProps) => {
       onClick={onClick}
     >
       <div className="flex items-center justify-center gap-2">
-        {loading && <img src={ring} width={24} height={24} alt="loading" />}
-        <p>Shorten</p>
+        {loading ? (
+          <img src={ring} width={24} height={24} alt="loading" />
+        ) : (
+          icon && <img src={icon} width={24} height={24} alt="icon" />
+        )}
+        <p>{text}</p>
       </div>
     </button>
   );
